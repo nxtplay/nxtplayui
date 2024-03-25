ccd5e45a4ec802f34ff7dedf1bbfee769c8cfd2e
Jamesduongrx <56172128+Jamesduongrx@users.noreply.github.com>
2024-03-24 17:56:10 -0700

    first changes

--- src/components/LoginView.js
+++ src/components/LoginView.js
@@ -1,31 +1,54 @@
 // src/components/LoginPage.js
-import React from 'react';
+import React, { useState } from 'react'; //"using the state hook"
 import { useNavigate } from 'react-router-dom';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword  } from "firebase/auth";
+import './LoginView.css';
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const auth = getAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        console.log(result);
-        navigate('/videos'); // Navigate to VideoPage after successful login
+        navigate('/dashboard');
       }).catch((error) => {
-        // Handle Errors here.
         console.error("Error during Google sign-in:", error.message);
       });
   };
+  
+   const handleLogin = () => {
+     signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+         navigate('/dashboard');
+       }).catch((error) => {
+        console.error("Error with email and password sign-in:", error.message);
+       });
+   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <button onClick={signInWithGoogle}>Sign In with Google</button>
+    <div className="login-page">
+      <div className="login-container">
+        <h2>Log In</h2>
+        <form onSubmit={handleLogin}>
+        <input type="email" placeholder="Email" value={email} onChange={(counter) => setEmail(counter.target.value)}   />
+          <input type="password" placeholder="Password" onChange={(counter) => setPassword(counter.target.value)} value={password}  />
+          <div className="login-actions">
+            <button type="submit" className="login-button">Log In</button>
+          </div>
+        </form>
+        <div className="separator">Or use</div>
+        <button className="google-sign-in" onClick={signInWithGoogle}>
+          Sign In with Google
+        </button>
+        <div className="register-link">
+          <p>Don't have an account? <a href="#">Register</a></p>
+        </div>
+      </div>
     </div>
   );
 };
-
 export default LoginPage;
